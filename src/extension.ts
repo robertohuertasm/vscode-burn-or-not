3c5bcd8bf6c7f30dc12e8a887d136ac9b37e135c
Roberto Huertas <roberto.huertas@outlook.com>
2023-12-02 20:28:56 +0100

    biome set

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { exec, spawn } from 'node:child_process';
 import * as path from 'node:path';
-import { spawn, exec } from 'node:child_process';
 import * as util from 'node:util';
+import * as vscode from 'vscode';
 
 const execPromise = util.promisify(exec);
 
@@ -16,12 +16,10 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.showInformationMessage('Burn or not is active!');
 
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      'burn-or-not.helloWorld',
-      () => {
-        vscode.window.showInformationMessage('Hello World from burn-or-not!');
-        executeBinary();
-      },
+    vscode.commands.registerCommand('burn-or-not.helloWorld', () => {
+      vscode.window.showInformationMessage('Hello World from burn-or-not!');
+      executeBinary();
+    }),
   );
 }
 
@@ -38,15 +36,14 @@ function executeBinary() {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const p = spawn(bin, { env: { NAME: 'burn-or-not' } });
 
-  p.stdout.on('data', (data: any) => {
+  p.stdout.on('data', (data: unknown) => {
     console.log(`Burn logs: ${data}`);
   });
 
-  p.on('close', (code: any) => {
+  p.on('close', (code: unknown) => {
     console.log(`Binary process exited with code ${code}`);
   });
 }
-
 
 function getBurnBinaryPath(): string {
   const platform = process.platform;
@@ -61,11 +58,10 @@ function getBurnBinaryPath(): string {
       'release',
       binName,
     );
-  } else {
-    let prodPath = path.join(__dirname, '..', 'bins', 'burn');
-    if (platform === 'darwin') {
-      prodPath = path.join(prodPath, 'mac');
-    }
-    return path.join(prodPath, binName);
   }
+  let prodPath = path.join(__dirname, '..', 'bins', 'burn');
+  if (platform === 'darwin') {
+    prodPath = path.join(prodPath, 'mac');
+  }
+  return path.join(prodPath, binName);
 }
